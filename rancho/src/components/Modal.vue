8dba78fdca54fa6fd2b9e63ddbff3fe9f1f85338
joesobo <soboleskijoe@gmail.com>
2023-03-06 14:57:25 -0800

    modal bounce and cowbell

--- rancho/src/components/Modal.vue
+++ rancho/src/components/Modal.vue
@@ -1,31 +1,40 @@
 <template>
-	<div class="min-w-[50px] min-h-[50px] fixed top-1/3 left-1/2 bg-black p-4" :class="!donationOverride && state ? 'flex flex-col justify-center' : 'hidden'" style="transform: translate(-50%, 0); border: 6px ridge #bbb; color: yellow;">
-		<!-- Close Button -->
-		<div class="flex justify-end">
-			<button @click="state=false" class="p-0 m-0">
-				<ul class="prohibited p-0 m-0">
-					<li class="align-items-start p-0 m-0"></li>
-				</ul>
-			</button>
-		</div>
+	<div>
+		<div v-if="!donationOverride && state" class="min-w-[50px] min-h-[50px] fixed top-1/3 left-1/2 bg-black p-4 flex flex-col" style="transform: translate(-50%, 0); border: 6px ridge #bbb; color: yellow;">
+			<!-- Close Button -->
+			<div class="flex justify-end">
+				<button @click="close()" class="p-0 m-0">
+					<ul class="prohibited p-0 m-0">
+						<li class="align-items-start p-0 m-0"></li>
+					</ul>
+				</button>
+			</div>
 
-		<!-- Image and Text -->
-		<div class="flex justify-between items-center">
-			<img src="images/dollars.gif" alt="dollar" />
-			<slot />
-		</div>
+			<!-- Image and Text -->
+			<div class="flex justify-between items-center">
+				<img src="images/dollars.gif" alt="dollar" />
+				<slot />
+			</div>
 
-		<!-- Donate -->
-		<div class="flex w-full justify-center">
+			<!-- Donate -->
 			<a
 				href="https://account.venmo.com/u/Ranchofest"
 				class="btn btn-primary btn-large mt-4"
 			>
 				<span class="blink">Donate</span>
 			</a>
+
+			<button class="mt-4 hover:underline" @click="overrideDonation()">I already donated!</button>
 		</div>
 
-		<button class="mt-4 hover:underline" @click="overrideDonation()">I already donated!</button>
+
+		<div v-if="!donationOverride && !state" class="w-full h-screen fixed">
+			<div class="el-wrap x">
+				<div class="el y">
+					<div class="h-[100px] flex justify-center items-center text-red-500 text-3xl font-bold p-4" style="background-color: yellow; border: 6px ridge #bbb;">Donate to Ranchofest!</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -38,10 +47,19 @@ const props = defineProps<{
 
 const donationOverride = localStorage.getItem('donationOverride')
 
+const sound = new Audio('sounds/cowbell.wav')
+
 const state = ref(props.open)
 
 const overrideDonation = () => {
 	state.value = false
 	localStorage.setItem('donationOverride', 'true')
+}
+
+const close = () => {
+	state.value = false
+
+	sound.loop = true;
+	sound.play();
 }
 </script>
