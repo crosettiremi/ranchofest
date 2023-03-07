1142a8071325140b4db337c589af9dfbf27e6da9
joesobo <soboleskijoe@gmail.com>
2023-03-07 12:13:00 -0800

    quick fixes

--- rancho/src/components/Modal.vue
+++ rancho/src/components/Modal.vue
@@ -1,9 +1,10 @@
 <template>
 	<div>
+		<!-- Modal -->
 		<div v-if="!donationOverride && state" class="min-w-[50px] min-h-[50px] fixed top-1/3 left-1/2 bg-black p-4 flex flex-col" style="transform: translate(-50%, 0); border: 6px ridge #bbb; color: yellow;">
 			<!-- Close Button -->
 			<div class="flex justify-end">
-				<button aria-label="close" @click="close()" class="p-0 m-0">
+				<button aria-label="close" @click="state = false" class="p-0 m-0">
 					<ul class="prohibited p-0 m-0">
 						<li class="align-items-start p-0 m-0"></li>
 					</ul>
@@ -28,14 +29,16 @@
 				<span class="blink">Donate</span>
 			</a>
 
+			<button aria-label="Terrible Person" class="mt-4 hover:underline" @click="close()">I'm a bad person and I won't be donating. Take me to the site I deserve.</button>
+
 			<button aria-label="Already Donated" class="mt-4 hover:underline" @click="overrideDonation()">I already donated!</button>
 		</div>
 
-
-		<div v-if="!donationOverride && !state" class="w-full h-screen fixed">
+		<!-- Bouncing Donation -->
+		<div v-if="badOverride" class="w-full h-screen fixed pointer-events-none">
 			<div class="el-wrap x">
-				<div class="el y">
-					<div class="h-[100px] flex justify-center items-center text-red-500 text-3xl font-bold p-4" style="background-color: yellow; border: 6px ridge #bbb;">Donate to Ranchofest!</div>
+				<div class="el y w-[250px] lg:w-[350px] h-[100px]">
+					<div class="h-[100px] flex justify-center items-center text-red-500 lg:text-3xl text-lg font-bold p-4" style="background-color: yellow; border: 6px ridge #bbb;">Donate to Ranchofest!</div>
 				</div>
 			</div>
 		</div>
@@ -49,6 +52,8 @@ const props = defineProps<{
 	open: boolean
 }>()
 
+let badOverride = ref(false)
+
 const donationOverride = localStorage.getItem('donationOverride')
 
 const sound = new Audio('sounds/cowbell.wav')
@@ -57,11 +62,13 @@ const state = ref(props.open)
 
 const overrideDonation = () => {
 	state.value = false
+	sound.pause();
 	localStorage.setItem('donationOverride', 'true')
 }
 
 const close = () => {
 	state.value = false
+	badOverride.value = true
 
 	sound.loop = true;
 	sound.play();
